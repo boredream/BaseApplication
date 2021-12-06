f2078fcdd378bdab2c8a54539d6c45721058f3e0
boredream <48262906@qq.com>
2021-12-06 11:00:13 +0800

    wheel dialog

--- app/src/main/java/com/boredream/baseapplication/view/DialogTitle.java
+++ app/src/main/java/com/boredream/baseapplication/view/DialogTitle.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,10 +13,8 @@ import com.boredream.baseapplication.R;
 
 public class DialogTitle extends ConstraintLayout {
 
-    private View close_view;
     private TextView title_tv;
     private View iconClose;
-    private Button buttonClose;
 
     public DialogTitle(Context context) {
         super(context);
@@ -41,23 +38,12 @@ public class DialogTitle extends ConstraintLayout {
         title_tv = findViewById(R.id.title);
         title_tv.setText(a.getString(R.styleable.DialogTitle_title));
         iconClose = findViewById(R.id.icon_close);
-        buttonClose = findViewById(R.id.button_close);
-        buttonClose.setText(a.getString(R.styleable.DialogTitle_close_text));
-
-        // 默认是按钮
-        if (a.getInt(R.styleable.DialogTitle_close_style, 0) == 0) {
-            close_view = buttonClose;
-            iconClose.setVisibility(INVISIBLE);
-        } else {
-            close_view = iconClose;
-            buttonClose.setVisibility(INVISIBLE);
-        }
         a.recycle();
     }
 
     @Override
     public void setOnClickListener(@Nullable OnClickListener l) {
-        close_view.setOnClickListener(l);
+        iconClose.setOnClickListener(l);
     }
 
     public void setTitle(String title) {
